d43d4ab4fb769b50211610a285b5b79aceda55be
Richard Forth <richard.forth@gmail.com>
2023-12-20 11:17:32 +0000

    Add files via upload

---
+++ variable_demo1.r
@@ -0,0 +1,17 @@
+# Short demo on variables and assignments
+
+# Assign some valuse to variables
+
+# Equal to operator
+FirstName = "Jackie"
+
+# Leftward Operator
+LastName = "Campbell"
+
+# Rightward Operator
+28 -> Age
+
+# Display all variables one by one using print
+print(FirstName)
+print(LastName)
+print(Age)
