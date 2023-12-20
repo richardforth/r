5fcf792337bfdf2c99707403d01527b5e3a73d3c
Richard Forth <richard.forth@gmail.com>
2023-12-20 13:17:42 +0000

    Add files via upload

---
+++ arithmetic_operators.r
@@ -0,0 +1,29 @@
+# Demo of Arithmetic Operators
+# Declare two variables, assign some values
+a = 125
+b = 78
+# Perform Arithmetic operations, assign result to variables
+# Calculate sum
+sum = a + b
+# Calculate difference
+difference = a - b
+# Calculate product
+product = a * b
+# Perform Division
+quotient = a / b
+# Find remainder
+mod = a %% b
+# Perform integer division
+int_quotient = a %/% b
+# Exponent
+expo = a ^ b
+# Display everything
+cat(" a = ", a , "b = ", b, "\n",
+   "sum = ", sum , "\n",
+   "difference = ", difference , "\n",
+   "product = ", product , "\n",
+   "quotient = ", quotient , "\n",
+   "remainder = ", mod , "\n",
+   "quotient (integer) = ", int_quotient , "\n",
+   "exponent = ", expo , "\n"
+  ) 
