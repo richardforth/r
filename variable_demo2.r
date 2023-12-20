7e7e88e019d589dfa6c1eef4c9bfd7c0da16efad
Richard Forth <richard.forth@gmail.com>
2023-12-20 11:25:19 +0000

    Add files via upload

---
+++ variable_demo2.r
@@ -0,0 +1,11 @@
+# Variables demo 2
+
+# Assign some values to variables
+
+First_Name = "Julia"
+Last_Name = "Hernandez"
+Age <- 14
+"Mexico" -> Country
+
+# Display everything using cat fuction
+cat("First Name: ", First_Name, "\nLast Name: ", Last_Name, "\nAge: ", Age, "\nCountry: ", Country, "\n")
