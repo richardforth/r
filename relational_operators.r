c533822b714560d0a810076e59375ae16169064e
Richard Forth <richard.forth@gmail.com>
2023-12-20 13:24:41 +0000

    Add files via upload

---
+++ relational_operators.r
@@ -0,0 +1,13 @@
+# Demo of relations operators
+# Declare two variables, assign some values
+a = 10
+b = 20
+# Perform Relational Operations and display directly using cat function
+cat(" a = ", a, "b = ", b, "\n",
+    "a == b : ", a == b , "\n",
+    "a != b : ", a != b , "\n",
+    "a < b : ", a < b , "\n",
+    "a > b : ", a > b , "\n",
+    "a <= b : ", a <= b , "\n",
+    "a >= b : ", a >= b , "\n"
+   ) 
